426d2e9a349769ae08ac5577a172d2c883d8e9dc
Joakim Englund <joakimenglund@protonmail.com>
2023-06-07 11:03:19 +0200

    Added comments to uart.h

--- uart.h
+++ uart.h
@@ -1,10 +1,11 @@
 #ifndef  __UART_H
-#define  __UART_H
+#define  __UART_H // Load this file if it's not already loaded
 
 
-#include "stm32f4xx.h"
-#include <stdio.h>
+#include "stm32f4xx.h" // Device specific header-file
+#include <stdio.h> // Standard IO
 
+// Declare two functions
 void USART2_Init(void);
 void test_setup(void);
 
