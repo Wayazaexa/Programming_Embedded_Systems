ad0fbcb54aae5a40be09ad1cf273a86657c81a95
Joakim Englund <joakimenglund@protonmail.com>
2023-06-07 11:02:22 +0200

    Added comments to main.c

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
-#include "uart.h"
+#include "uart.h" // Include the UART code
 
 
-int main()
+int main() // Begin the main function
 {
 
-    USART2_Init();
+    USART2_Init(); // Call the init-function
 
     //test_setup();
-    printf("Hello there from main function");
-    while (1) {
+    printf("Hello there from main function"); // Print to standard output
+    while (1) { // Infinite loop
 
     }
 
